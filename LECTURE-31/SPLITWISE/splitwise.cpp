cc38db8f6cbcf14a43d2c1d0dacc1251c03cfc28
agrimkapoor <agrimkapoor07@gmail.com>
2026-08-24 09:48:45 +0530

    Create splitwise.cpp

---
+++ LECTURE-31/SPLITWISE/splitwise.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class User{
+    public:
+    string ID,
+    string name,
+    string email,
+    map<string,double>balances
+};
+
+int main(){
+  return 0;
+}
